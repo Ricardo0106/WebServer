46a2907dc697ebf46d61512581eff0393d9c58d4
李嘉图 <837332546@qq.com>
2023-09-30 11:02:20 +0800

    Update Epoll.h

--- Epoll.h
+++ Epoll.h
@@ -1,5 +1,8 @@
-// @Author Lin Ya
-// @Email xxbbb@vip.qq.com
+/*
+ * @Author: Zhibin Yin
+ * @Date:   2022-05-24
+ * @copyleft Apache 2.0
+ */ 
 #pragma once
 #include <sys/epoll.h>
 #include <memory>
